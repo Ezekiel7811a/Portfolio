299ef9c4ffae1c55bc7a7771549a8aef9e847913
Ezekiel7811a <vincentcharles.philippe@gmail.com>
2023-06-21 19:53:25 +0200

    lot of erase

--- JavaScript/Meta-Stasis/back/scrap/spells/writing.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def writeData(filename, data, indent=3):
-    try:
-        file = open(filename, 'w', encoding='utf-8')
-    except:
-        file = open(filename, 'x', encoding='utf-8')
-    
-    json.dump(data, file, indent=indent, ensure_ascii=False)
